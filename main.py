c03c654de2afbd5c86359cf563ef14fbc61bb7fc
xxxfhy <40598925+xxxfhy@users.noreply.github.com>
2023-02-06 15:31:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,18 +14,17 @@ def get_file_name(path):
             filenameList.append(filename)  # 返回mkv mp4文件名（不包含扩展名）
         elif filetype in subtypeList:
             subtitleList.append(filename)  # 返回字幕文件名（包含扩展名）
-    return filenameList, subtitleList
+    return filenameList, subtitleList, filetype
 
 
-def rename(filenameList, subtitleList):
+def rename(filenameList, subtitleList, filetype):
     for i, filename in enumerate(subtitleList):  # 遍历所有字幕文件
-        # subtype = os.path.splitext(filename)[1] ??????????????????????
-        newName = os.path.join(filenameList[i]+".ass") #path ????????????????????????
-        oldName = os.path.join(filename+".ass") #后缀 ?????????????????????????????
+        newName = os.path.join(filenameList[i]+filetype) 
+        oldName = os.path.join(filename+filetype) 
         os.rename(oldName, newName)
 
 
-# 若main()函数无输入则path为当前目录
+# 若main()函数无输入则path为脚本所在目录
 def main(path=os.path.split(os.path.realpath(__file__))[0]):
     os.chdir(path)
     filename_list, subtitle_list = get_file_name(path)
@@ -33,6 +32,5 @@ def main(path=os.path.split(os.path.realpath(__file__))[0]):
 
 
 if __name__ == '__main__':
-    main()  # os.curdir
+    main()
     print(os.path.split(os.path.realpath(__file__))[0])
-    
